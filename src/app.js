b1ea0ea3fb24410abb5ed6bdbeda956638f2a181
Mushlya <olga.synevir@gmail.com>
2022-10-06 23:24:27 +0200

    Update time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+function formatDayTime(timestamp)
+{
+let date=new Date(timestamp);
+let hour=date.getHours();
+if (hour < 10)
+{
+    hour = "0" + hour;
+}
+let minute=date.getMinutes();
+if (minute < 10)
+{
+    minute = "0" + minute;
+}
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day=days[date.getDay()];
+return "Last updated: " + day + " " + hour + ":" + minute;
+}
+
 function displayTemperature(response)
 {
 console.log(response.data.main.temp);
@@ -6,11 +24,19 @@ let cityElement=document.querySelector("#apiCity");
 let conditionsElement=document.querySelector("#apiConditions");
 let humidityElement=document.querySelector("#apiHumidity");
 let windElement=document.querySelector("#apiWind");
+let dayTimeElement=document.querySelector("#apiDayTime");
 tempElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 conditionsElement.innerHTML=response.data.weather[0].description;
 humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=Math.round(response.data.wind.speed);
+dayTimeElement.innerHTML=formatDayTime(response.data.dt * 1000);
+
+
+
+
+
+
 }
 
 let apiKey = "0cf6b60d58b0c697532c33384fe20a26";
